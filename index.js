8f9968c28f8064e62b629e5fad27fc54a3399f51
Emmanuel Quentin <manu.quentin@gmail.com>
2023-01-19 09:40:06 -0500

    Apply some fixes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// FIXME: Don't search on SCCP line for SIP result, we loop over the each line and we don't care about the type of line. If for a user we have a sccp line we don't need to make an http GET to get his SIP line because it's an sccp line. (needed)
-// FIXME: There is no documentation for onCardCanceled (needed)
-// FIXME: Tooltip seems not working everywhere to get information for debugging. Exemple on icon, there is only tooltip on the first icon. (needed)
-// FIXME: When i use the click to call, the call is not launched now (needed)
-// FIXME: Calllog seems not updated after a call, need to reload the page (needed)
-// FIXME if we use loginWithToken, the method onAuthenticate is call twice. (needed)
+// [x] FIXME: Don't search on SCCP line for SIP result, we loop over the each line and we don't care about the type of line. If for a user we have a sccp line we don't need to make an http GET to get his SIP line because it's an sccp line. (needed)
+// [x] FIXME: There is no documentation for onCardCanceled (needed)
+// [x] FIXME: Tooltip seems not working everywhere to get information for debugging. Exemple on icon, there is only tooltip on the first icon. (needed)
+// [?] FIXME: When i use the click to call, the call is not launched now (needed)
+// [x] FIXME: Calllog seems not updated after a call, need to reload the page (needed)
+// [x] FIXME if we use loginWithToken, the method onAuthenticate is call twice. (needed)
 
 // FIXME: Permit to support CTI instead webrtc lines (not now) (not needed)
 // FIXME: UX/UI, make a connection page with all information for login a user. Like add a connection and use it for the authentication. (exemple with okta, no a priority, but good idea for the end user) (not needed)
@@ -34,11 +34,11 @@ const getSessionOnStorage = () => JSON.parse(localStorage.getItem('session'));
 
 const setSessionOnStorage = session => {
   localStorage.setItem('session', JSON.stringify(session));
-}
+};
 
 const removeSessionOnStorage = () => {
   localStorage.clear();
-}
+};
 
 const openLogin = () => {
   $('#authentication').show();
@@ -52,16 +52,16 @@ const openLogin = () => {
 
     authenticate($('#email').val(), $('#password').val());
   });
-}
+};
 
 const checkJson = item => {
   try {
-    const message = JSON.parse(item)
+    const message = JSON.parse(item);
     return message.reason || item;
-  } catch(_) {
+  } catch (_) {
     return item;
   }
-}
+};
 
 const displayAuthError = error => {
   $('.auth-error').html('');
@@ -81,38 +81,17 @@ const displayAuthError = error => {
     $('#server').addClass('onalert');
     $('.serv-lab').addClass('onerror');
   }
-  
+
   $('#submit-login').prop('disabled', false);
   $('.login-txt').html('login');
-}
+};
 
-const authenticate = async (username, password) => {
-  try {
-    const session = await Wazo.Auth.logIn(username, password).catch();
-    setSessionOnStorage(session);
-
-    onLogin();
-  } catch (e) {
-    displayAuthError(e);
-  }
-}
-
-const onLogin = () => {
-  $('#authentication').hide();
-  $('#app').show();
-  $('#logout').on('click', () => {
-    removeSessionOnStorage();
-    window.location.reload(false);
-  });
-
-  app();
-}
+// This should not be in `app` because it's fired after `softphone.init`.
+softphone.onIFrameLoaded = () => {
+  button.disabled = false;
+};
 
 const app = () => {
-  softphone.onIFrameLoaded = () => {
-    button.disabled = false;
-  }
-
   softphone.optionsFetched('clientId', [
     { label: 'Manu', id: 'test' },
     { label: 'Bob', id: '123' },
@@ -141,51 +120,51 @@ const app = () => {
       clientId: { type: 'object', title: 'Client ID' },
       vegetarian: { type: 'boolean' },
       nationality: {
-        "type": "string",
-        "enum": [
-          "DE",
-          "IT",
-          "JP",
-          "US",
-          "RU",
-          "Other"
-        ]
+        type: 'string',
+        enum: [
+          'DE',
+          'IT',
+          'JP',
+          'US',
+          'RU',
+          'Other',
+        ],
       },
     },
   }, {
     note: { 'ui:widget': 'textarea' },
-    clientId :{ 'ui:field': 'autocomplete'},
+    clientId: { 'ui:field': 'autocomplete' },
   });
 
   softphone.onCallEstablished = (call) => {
     console.log(call);
     result.innerHTML = 'onCallEstablished';
-  }
+  };
 
   softphone.onCallRejected = (call) => {
     console.log(`reject ${call}`);
     result.innerHTML = 'Reject';
-  }
+  };
 
   softphone.onCardSaved = card => {
     result.innerHTML = JSON.stringify(card);
-  }
+  };
 
   softphone.onLinkEnabled = link => {
     link.style.textDecorationStyle = 'dotted';
-  }
+  };
 
   softphone.onCallIncoming = call => {
     status.innerHTML = `Call incoming from ${call.displayName}`;
     softphone.displaySoftphone();
     new Notification(`Call incoming from ${call.displayName}`);
-  }
+  };
 
   softphone.onCallEnded = (call, card) => {
     status.innerHTML = `Call with ${call.displayName} ended`;
     result.innerHTML = `Call with ${call.displayName} ended, card: ${JSON.stringify(card)}`;
     softphone.setCardValue('cardId', 'some-card-id');
-  }
+  };
 
   softphone.onOutgoingCallMade = call => {
     result.innerHTML = `<code>${JSON.stringify(call)}</code>`;
@@ -201,98 +180,120 @@ const app = () => {
     setTimeout(() => {
       softphone.setCardValue('vegetarian', false);
     }, 4000);
-  }
+  };
 
   softphone.onAuthenticated = async (session) => {
+    console.log('onAuthenticated', session);
     status.innerHTML = `Hello ${session.profile.firstName}`;
-  
+
     Wazo.Auth.setHost(wazoServer);
     Wazo.Auth.setApiToken(session.token);
-  
-    const callLogs = await Wazo.api.callLogd.listCallLogs();
-    console.log(callLogs);
-  }
+
+    // const callLogs = await Wazo.api.callLogd.listCallLogs();
+  };
 
   softphone.onDtmfSent = tone => {
     console.log(tone);
     result.innerHTML = tone;
-  }
+  };
 
   softphone.onLoggedOut = session => {
     console.log(session);
     result.innerHTML = session;
-  }
+  };
 
   softphone.onStartRecording = () => {
     console.log('START RECORD');
-  }
+  };
 
   softphone.onStopRecording = () => {
     console.log('STOP RECORD');
-  }
+  };
 
   softphone.onAgentLoggedIn = () => {
     console.log('Agent Logged in');
-  }
+  };
 
   softphone.onAgentLoggedOut = () => {
     console.log('Agent Logged out');
-  }
+  };
 
   softphone.onAgentPaused = () => {
     console.log('Agent Pause');
-  }
+  };
 
   softphone.onAgentResumed = () => {
     console.log('Agent Resumed');
-  }
+  };
 
   softphone.onLanguageChanged = (language) => {
     console.log(language);
-  }
+  };
 
   softphone.onCallHeld = () => {
     console.log('Call Held');
-  }
+  };
 
   softphone.onCallResumed = () => {
     console.log('Call resumed');
-  }
+  };
 
   softphone.onCallMuted = () => {
     console.log('Call muted');
-  }
+  };
 
   softphone.onCallUnMuted = () => {
     console.log('Call unmuted');
-  }
+  };
 
   softphone.onWazoContactSearch = query => {
     console.log(`Searching contact... ${query}`);
-  }
+  };
 
   softphone.onCardCanceled = () => {
-    console.log('OncardCanceled');
-  }
+    console.log('onCardCanceled');
+  };
 
   softphone.onCallLogCreated = cdr => {
     console.log('onCallLogCreated');
     console.log(cdr);
-  }
+  };
 
   softphone.onWebsocketMessage = (message) => {
-    console.log(message);
-  }
+    console.log('onWebsocketMessage', message);
+  };
 
   softphone.onSearchOptions = (fieldId, query) => {
     console.log(fieldId);
     console.log(query);
     const results = [
       { label: 'Tutu', id: '0123' },
-    ]
+    ];
     softphone.onOptionsResults(fieldId, results);
+  };
+};
+
+const onLogin = () => {
+  $('#authentication').hide();
+  $('#app').show();
+  $('#logout').on('click', () => {
+    removeSessionOnStorage();
+    window.location.reload(false);
+  });
+
+  app();
+};
+
+const authenticate = async (username, password) => {
+  try {
+    const session = await Wazo.Auth.logIn(username, password).catch();
+    setSessionOnStorage(session);
+
+    onLogin();
+  } catch (e) {
+    displayAuthError(e);
   }
-}
+};
 
 (async () => {
   Wazo.Auth.init('wazo-softphone-demo');
@@ -303,12 +304,13 @@ const app = () => {
     return openLogin();
   }
 
+  // We should call `init` even if we don't have a session.
+  softphone.init({ url: 'http://localhost:3000', server: wazoServer, domainName: domainNameLdap, debug, disableAutoLogin: true });
+
   const session = await Wazo.Auth.validateToken(rawSession.token, rawSession.refreshToken);
   if (session) {
-    softphone.init({server: wazoServer, domainName: domainNameLdap, debug: debug});
     softphone.loginWithToken(session.token, rawSession.refreshToken);
   }
 
-  onLogin();
-
+  return onLogin();
 })();
